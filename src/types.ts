e21f1acaf10f310d88ff631b315226af63852815
srednicki95 <srednicki95@gmail.com>
2020-02-04 00:23:34 +0100

    feat: extend IAzureMap type childers by IAzureMapChildren

--- src/types.ts
+++ src/types.ts
@@ -31,7 +31,7 @@ export type IAzureMapChildren =
   | ReactElement<IAzureMapDataSourceProps>
 
 export type IAzureMap = {
-  children?: Array<IAzureMapChildren>
+  children?: Array<IAzureMapChildren> | IAzureMapChildren
   providedMapId?: string
   containerClassName?: string
   LoaderComponent?: ComponentClass<any> | StatelessComponent<any>
