1861364bcb0b0b793e7ca868eaf96fb9cbe7a80f
Maciej Sasinowski <38035075+msasinowski@users.noreply.github.com>
2020-03-04 15:06:45 +0100

    fix: handle options change for popup (#37)
    * fix: handle options change for popup and close/open popup
    
    Co-authored-by: Patryk Średnicki <41010528+srednicki95@users.noreply.github.com>

--- src/components/AzureMapPopup/useCreateAzureMapPopup.ts
+++ src/components/AzureMapPopup/useCreateAzureMapPopup.ts
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import atlas from 'azure-maps-control'
 
-import { IAzureMapPopup } from '../../types'
+import { IAzureMapPopup, IAzureMapsContextProps } from '../../types'
+import { AzureMapsContext } from '../../contexts/AzureMapContext'
 export const useCreatePopup = ({
   options,
-  popupContent
-}: Pick<IAzureMapPopup, 'popupContent' | 'options'>) => {
+  popupContent,
+  isVisible
+}: Pick<IAzureMapPopup, 'popupContent' | 'options' | 'isVisible'>) => {
   const [popupRef] = useState<atlas.Popup>(
     new atlas.Popup({ ...options, content: renderToStaticMarkup(popupContent) })
   )
+  const { mapRef } = useContext<IAzureMapsContextProps>(AzureMapsContext)
+
+  useEffect(() => {
+    popupRef.setOptions({
+      ...options,
+      content: renderToStaticMarkup(popupContent)
+    })
+    if (mapRef && isVisible && !popupRef.isOpen()) {
+      popupRef.open(mapRef)
+    }
+  }, [options, popupContent])
+
   return popupRef
 }
 
