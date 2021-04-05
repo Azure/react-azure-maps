614dee004bfe2df5576360b00fbe7fbb2ffc143b
ambientlight <primary.taras.vozniuk@gmail.com>
2021-04-06 01:48:26 +0800

    fix: handle VectorTileSource in useFeature

--- src/components/AzureMapFeature/useFeature.ts
+++ src/components/AzureMapFeature/useFeature.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { DataSourceType, IAzureMapFeature, ShapeType, FeatureType } from '../../types'
 import { useCheckRef } from '../../hooks/useCheckRef'
+import atlas from 'azure-maps-control'
 
 export const useFeature = (
   { setCoords, setProperties }: IAzureMapFeature,
@@ -8,21 +9,28 @@ export const useFeature = (
   shapeRef: ShapeType | null,
   featureRef: FeatureType | null
 ) => {
-  // Simple feature's usecases and methods
-  useCheckRef<DataSourceType, FeatureType>(dataSourceRef, featureRef, (dref, fref) => {
-    dref.add(fref)
-    return () => {
-      dref.remove(fref)
-    }
-  })
+  if(dataSourceRef instanceof atlas.source.DataSource){
+    // Simple feature's usecases and methods
+    useCheckRef<atlas.source.DataSource, FeatureType>(dataSourceRef, featureRef, (dref, fref) => {
+      dref.add(fref)
+      return () => {
+        dref.remove(fref)
+      }
+    })
 
-  // Shape's usecases and methods
-  useCheckRef<DataSourceType, ShapeType>(dataSourceRef, shapeRef, (dref, sref) => {
-    dref.add(sref)
-    return () => {
-      dref.remove(sref)
-    }
-  })
+    // Shape's usecases and methods
+    useCheckRef<atlas.source.DataSource, ShapeType>(dataSourceRef, shapeRef, (dref, sref) => {
+      dref.add(sref)
+      return () => {
+        dref.remove(sref)
+      }
+    })
+  } else if (dataSourceRef instanceof atlas.source.VectorTileSource) {
+    useCheckRef<FeatureType, FeatureType>(featureRef, featureRef, (fref) => 
+      //NOTE: IAzureVectorTileSourceChildren won't allow adding AzureMapFeature as a child, still notify in case type check was suppresed
+      console.error(`Unable to add Feature(${fref.id}) to VectorTileSource(${dataSourceRef.getId()}): AzureMapFeature has to be a child of AzureMapDataSourceProvider`)
+    )
+  }
 
   useEffect(() => {
     if (shapeRef && setCoords) {
