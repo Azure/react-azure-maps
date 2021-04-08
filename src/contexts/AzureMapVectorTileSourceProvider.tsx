853b35e6a813aa838ed7a6fdd2e9bcc52b2d396c
ambientlight <primary.taras.vozniuk@gmail.com>
2021-04-09 00:20:29 +0800

    fix: vector tile source provider types, typedoc comments

--- src/contexts/AzureMapVectorTileSourceProvider.tsx
+++ src/contexts/AzureMapVectorTileSourceProvider.tsx
@@ -1,22 +1,28 @@
 import atlas from 'azure-maps-control'
 import React, { useContext, useState } from 'react'
 import { useCheckRef } from '../hooks/useCheckRef'
-import { DataSourceType, IAzureDataSourceStatefulProviderProps, IAzureMapsContextProps, MapType } from '../types'
+import { DataSourceType, IAzureDataSourceStatefulProviderProps, IAzureMapLayerEventType, IAzureMapsContextProps, IAzureMapSourceEventType, IAzureVectorTileSourceStatefulProviderProps, MapType } from '../types'
 import { AzureMapDataSourceRawProvider as Provider } from './AzureMapDataSourceContext'
 import { AzureMapsContext } from './AzureMapContext'
 
+/**
+ * @param id datasource identifier
+ * @param children layer providers representing datasource layers
+ * @param options vector tile datasource options: see atlas.VectorTileSourceOptions
+ * @param events a object containing sourceadded, sourceremoved event handlers
+ */
 const AzureMapVectorTileSourceStatefulProvider = ({
   id,
   children,
   options,
   events,
-}: IAzureDataSourceStatefulProviderProps) => {
+}: IAzureVectorTileSourceStatefulProviderProps) => {
   const [dataSourceRef] = useState<atlas.source.VectorTileSource>(new atlas.source.VectorTileSource(id, options))
   const { mapRef } = useContext<IAzureMapsContextProps>(AzureMapsContext)
   useCheckRef<MapType, DataSourceType>(mapRef, dataSourceRef, (mref, dref) => {
-    for (const eventType in events || {}) {
-      mref.events.add(eventType as any, dref, events[eventType])
-    }
+    Object.entries(events || {}).filter(([_, handler]) => handler !== undefined).forEach(([eventType, handler]) => {
+      mref.events.add(eventType as IAzureMapSourceEventType, dref, handler as (e: atlas.source.Source) => void)
+    })
     mref.sources.add(dref)
   })
 
