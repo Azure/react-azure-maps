ac2281571f5f1ce793fb79566ca61665a4f1b00c
Ronald van der Kooij <ronald.van.der.kooij@nu.nl>
2022-09-11 22:53:04 +0200

    fix: also cleaning up datasources in azuremapvectortilesourceprovider

--- src/contexts/AzureMapVectorTileSourceProvider.test.tsx
+++ src/contexts/AzureMapVectorTileSourceProvider.test.tsx
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react'
 import React, { useContext } from 'react'
-import { Map } from 'azure-maps-control'
+import atlas, { layer, Map } from 'azure-maps-control'
 import { IAzureVectorTileSourceStatefulProviderProps } from '../types'
 import { AzureMapsContext } from './AzureMapContext'
 import { AzureMapVectorTileSourceProvider } from './AzureMapVectorTileSourceProvider'
@@ -70,5 +70,42 @@ describe('AzureMapVectorTileSourceProvider tests', () => {
       expect.any(Object),
       expect.any(Function)
     )
+  })
+
+  it('should remove data source from the map ref on unmount', () => {
+    mapRef.events.remove = jest.fn()
+    const events = { sourceadded: () => {} }
+    const { unmount, result } = renderHook(() => useContextConsumer(), {
+      wrapper: wrapWithVectorTileSourceContext({ id: 'id', options: { option: 'option' }, events })
+    })
+
+    unmount()
+
+    expect(mapRef.sources.remove).toHaveBeenCalledWith(result.current.dataSourceRef)
+    expect(mapRef.events.remove).toHaveBeenCalledWith(
+      'sourceadded',
+      result.current.dataSourceRef,
+      events.sourceadded
+    )
+  })
+
+  it('should remove all layers that are using the same datasource from the map ref on unmount', () => {
+    const dsToBeRemoved = new atlas.source.DataSource('ds_to_be_removed')
+    const dsToKeep = new atlas.source.DataSource('ds_to_keep')
+
+    const symbolLayer = new layer.SymbolLayer(dsToBeRemoved, 'layer_to_be_removed')
+    const bubbleLayer = new layer.BubbleLayer(dsToKeep, 'layer_to_keep')
+
+    symbolLayer.getSource = jest.fn(() => dsToBeRemoved)
+
+    mapRef.layers.getLayers = jest.fn(() => [symbolLayer, bubbleLayer])
+
+    const { unmount } = renderHook(() => useContextConsumer(), {
+      wrapper: wrapWithVectorTileSourceContext({ id: dsToBeRemoved.getId() })
+    })
+
+    unmount()
+    expect(mapRef.layers.remove).toHaveBeenCalledTimes(1)
+    expect(mapRef.layers.remove).toHaveBeenNthCalledWith(1, 'layer_to_be_removed')
   })
 })
--- src/contexts/AzureMapVectorTileSourceProvider.tsx
+++ src/contexts/AzureMapVectorTileSourceProvider.tsx
@@ -1,7 +1,13 @@
 import atlas from 'azure-maps-control'
 import React, { useContext, useState } from 'react'
 import { useCheckRef } from '../hooks/useCheckRef'
-import { DataSourceType, IAzureMapsContextProps, IAzureMapSourceEventType, IAzureVectorTileSourceStatefulProviderProps, MapType } from '../types'
+import {
+  DataSourceType,
+  IAzureMapsContextProps,
+  IAzureMapSourceEventType,
+  IAzureVectorTileSourceStatefulProviderProps,
+  MapType
+} from '../types'
 import { AzureMapDataSourceRawProvider as Provider } from './AzureMapDataSourceContext'
 import { AzureMapsContext } from './AzureMapContext'
 
@@ -15,24 +21,55 @@ const AzureMapVectorTileSourceStatefulProvider = ({
   id,
   children,
   options,
-  events = {},
+  events = {}
 }: IAzureVectorTileSourceStatefulProviderProps) => {
-  const [dataSourceRef] = useState<atlas.source.VectorTileSource>(new atlas.source.VectorTileSource(id, options))
+  const [dataSourceRef] = useState<atlas.source.VectorTileSource>(
+    new atlas.source.VectorTileSource(id, options)
+  )
   const { mapRef } = useContext<IAzureMapsContextProps>(AzureMapsContext)
   useCheckRef<MapType, DataSourceType>(mapRef, dataSourceRef, (mref, dref) => {
     for (const eventType in events) {
-      const handler = events[eventType as IAzureMapSourceEventType] as (e: atlas.source.Source) => void | undefined
-      if(handler) {
+      const handler = events[eventType as IAzureMapSourceEventType] as (
+        e: atlas.source.Source
+      ) => void | undefined
+      if (handler) {
         mref.events.add(eventType as IAzureMapSourceEventType, dref, handler)
       }
     }
     mref.sources.add(dref)
+
+    return () => {
+      for (const eventType in events || {}) {
+        const handler = events[eventType as IAzureMapSourceEventType] as (
+          e: atlas.source.Source
+        ) => void | undefined
+        if (handler) {
+          mref.events.remove(eventType as IAzureMapSourceEventType, dref, handler)
+        }
+      }
+
+      const getLayersDependingOnDatasource = (dst: DataSourceType) => {
+        return mref.layers.getLayers().filter((l) => {
+          if ((l as atlas.layer.SymbolLayer).getSource) {
+            const sourceLayer = (l as atlas.layer.SymbolLayer).getSource()
+            const dsId = typeof sourceLayer === 'string' ? sourceLayer : sourceLayer.getId()
+            return dsId === dst.getId()
+          }
+          return false
+        })
+      }
+
+      getLayersDependingOnDatasource(dref).forEach((l) => {
+        mref.layers.remove(l.getId() ? l.getId() : l)
+      })
+      mref.sources.remove(dref)
+    }
   })
 
   return (
     <Provider
       value={{
-        dataSourceRef,
+        dataSourceRef
       }}
     >
       {mapRef && children}
