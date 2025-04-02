a90d3827075a1368bce31dae779d4abde74e6bc7
Savina Shen (Manpower Services Taiwan Co Ltd) <a-savinashen@microsoft.com>
2025-04-02 18:00:24 +0800

    fix: add popups once map is ready

--- src/components/AzureMapPopup/useCreateAzureMapPopup.ts
+++ src/components/AzureMapPopup/useCreateAzureMapPopup.ts
@@ -2,8 +2,9 @@ import { useState, useEffect, useContext } from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import atlas from 'azure-maps-control'
 
-import { IAzureMapPopup, IAzureMapsContextProps } from '../../types'
+import { IAzureMapPopup, IAzureMapsContextProps, MapType } from '../../types'
 import { AzureMapsContext } from '../../contexts/AzureMapContext'
+import { useCheckRef } from '../../hooks/useCheckRef'
 export const useCreatePopup = ({
   options,
   popupContent,
@@ -14,6 +15,10 @@ export const useCreatePopup = ({
   )
   const { mapRef } = useContext<IAzureMapsContextProps>(AzureMapsContext)
 
+  useCheckRef<MapType, MapType>(mapRef, mapRef, mref => {
+    mref.events.addOnce('ready', () => mref.popups.add(popupRef))
+  })
+  
   useEffect(() => {
     popupRef.setOptions({
       ...options,
